aab2ff64e96f91b14d470b9983e1ab27d554f188
Negin <nuggetnegin@gmail.com>
2020-02-03 17:24:43 -0500

    setup linter and added more useless functionality

--- ribbot.js
+++ ribbot.js
@@ -1,23 +1,23 @@
 // require the discord.js module
 const Discord = require('discord.js');
-/*destructuring to extract prefix variables*/
+/* destructuring to extract prefix variables */
 const { prefix, token } = require('./config.json');
 
 // create a new Discord client
 const client = new Discord.Client();
-
+// login to Discord with your app's token
+client.login(token);
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+        console.log('Ready!');
 });
 
-// login to Discord with your app's token
-client.login(token);
-
 client.on('message', message => {
-    if(message.content ==='wassap')
-    message.channel.send('not much');
-	console.log(message.content);
+        if (message.content.startsWith(`${prefix}hello`) || message.content.startsWith(`${prefix}hi`)) {
+                message.channel.send('hi!!');
+        } else if (message.content.startsWith(`${prefix}bye`) || message.content.startsWith(`${prefix}goodbye`)) {
+                message.channel.send('bye bye');
+        }
 });
